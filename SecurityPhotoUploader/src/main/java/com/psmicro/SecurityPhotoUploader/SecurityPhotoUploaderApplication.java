df3bf1664baf77c6b59472fcfa8474bf6067a2e7
Prakhar151202 <116758420+Prakhar151202@users.noreply.github.com>
2024-04-06 00:45:42 +0530

    Add files via upload

---
+++ SecurityPhotoUploader/src/main/java/com/psmicro/SecurityPhotoUploader/SecurityPhotoUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.psmicro.SecurityPhotoUploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPhotoUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityPhotoUploaderApplication.class, args);
+	}
+
+}
---
+++ SecurityPhotoUploader/src/test/java/com/psmicro/SecurityPhotoUploader/SecurityPhotoUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psmicro.SecurityPhotoUploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPhotoUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
